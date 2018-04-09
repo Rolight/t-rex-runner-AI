26df92b0168dc2a1ed94528971b9cd6e28595d4d
rolight <flsnnx@gmail.com>
2018-04-09 20:11:44 +0800

    add databuffer code

--- train.py
+++ train.py
@@ -1,187 +1,25 @@
+import datetime
 import tensorflow as tf
+import pickle
+import json
 import numpy as np
 
-from utils import build_mlp
+from models import NNDynamicModel, MPCcontroller
+from utils import load_sample_data, DataBuffer
 
-ENV_CONF = {
-    'action_dim': 2,
-    'action_type_size': 3,
-    'action_hold_time_limit': 1,
-    'observation_dim': 5
+DYNMAMIC_MODEL_PARAMS = {
+    'regularizer': tf.contrib.layers.l2_regularizer(scale=1e-5),
+    'activation': tf.nn.relu,
+    'output_activation': None,
+    'learning_rate': 0.003,
+    'batch_size': 128,
+    'n_layers': 3,
+    'size': 128,
+    'iterations': 40
 }
 
 
-class NNDynamicModel:
-
-    def __init__(self,
-                 name,
-                 n_layers,
-                 size,
-                 activation,
-                 output_activation,
-                 kernel_regularizer,
-                 normalization,
-                 batch_size,
-                 iterations,
-                 learning_rate,
-                 sess,
-                 env_conf=ENV_CONF
-                 ):
-        # base parameters
-        self.batch_size = batch_size
-        self.sess = sess
-        self.normalization = normalization
-        self.iter = iterations
-        self.name = name
-        self.env_conf = env_conf
-
-        self.mlp_params = {
-            'scope': 'nndym-%s' % name,
-            'n_layers': n_layers,
-            'size': size,
-            'activation': activation,
-            'output_activation': output_activation,
-            'kernel_regularizer': kernel_regularizer
-        }
-        self.learning_rate = learning_rate
-        self.initialized = False
-
-        self.build_graph()
-
-    def build_graph(self):
-        self.add_placeholder()
-        self.pred = self.add_prediction_op()
-        self.loss = self.add_loss_op(self.pred)
-        self.train_op = self.add_train_op(self.loss)
-
-    def add_placeholder(self):
-        # input [observation, action]
-        self.input_placeholder = tf.placeholder(
-            dtype=tf.float32,
-            shape=[None, self.env_conf['observation_dim'] +
-                   self.env_conf['action_dim']]
-        )
-
-        # label [observation_delta, reward]
-        self.label_placeholder = tf.placeholder(
-            dtype=tf.float32,
-            shape=[None, self.env_conf['observation_dim'] + 1]
-        )
-
-    def add_prediction_op(self):
-        inputs = self.input_placeholder
-        pred = build_mlp(
-            input_placeholder=inputs,
-            output_size=self.label_placeholder.shape[1],
-            **self.mlp_params
-        )
-        return pred
-
-    def add_loss_op(self, pred):
-        loss = tf.reduce_mean(tf.square(
-            pred - self.label_placeholder
-        ))
-        return loss
-
-    def add_train_op(self, loss):
-        train_op = tf.train.AdamOptimizer(
-            learning_rate=self.learning_rate
-        ).minimize(loss)
-        return train_op
-
-    def normalize_obs(self, obs):
-        min_obs, max_obs, mean_obs = self.normalization
-        obs[:, 1:] = (obs[:, 1:] - mean_obs[1:]) / (max_obs[1:] - min_obs[1:])
-        return obs
-
-    def recover_obs(self, obs):
-        min_obs, max_obs, mean_obs = self.normalization
-        obs[:, 1:] = obs[:, 1:] * (max_obs[1:] - min_obs[1:]) + mean_obs[1:]
-        return obs
-
-    def fit(self, data):
-        """
-        data format: [[obs, action, next_obs, reward]]
-        """
-        inputs_data = np.array([d[0] + d[1] for d in data])
-        obs_len = len(data[0][0])
-        inputs_data[:, :obs_len] = self.normalize_obs(inputs_data[:, :obs_len])
-        outputs_data = np.array([d[2] + [d[3]] for d in data])
-        outputs_data[:, :obs_len] = self.normalize_obs(
-            outputs_data[:, :obs_len])
-        train_indicies = np.arange(len(data))
-        for iter_step in range(self.iter):
-            np.random.shuffle(train_indicies)
-            losses = []
-            for i in range(len(train_indicies) // self.batch_size):
-                start_id = i * self.batch_size
-                train_ids = train_indicies[start_id: start_id+self.batch_size]
-                input_batch = inputs_data[train_ids]
-                output_batch = outputs_data[train_ids]
-
-                loss, _ = self.sess.run([self.loss, self.train_op], feed_dict={
-                    self.input_placeholder: input_batch,
-                    self.label_placeholder: output_batch,
-                })
-                losses.append(loss)
-            print('on iter_step %d, loss = %0.7f' %
-                  (iter_step, np.mean(losses)))
-
-    def predict(self, states, actions):
-        states = np.array(states)
-        states = self.normalize_obs(states)
-        inputs = np.concatenate([states, actions], axis=1)
-        pred = self.sess.run(self.pred, feed_dict={
-            self.input_placeholder: inputs
-        })
-        pred[:, :-1] = self.recover_obs(pred[:, :-1])
-        nxt_states, rewards = pred[:, :-1], pred[:, -1]
-        return nxt_states, rewards
+now_datetime = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
 
 
-class MPCcontroller:
-
-    def __init__(self, dyn_model, sample_size=20, env_conf=ENV_CONF):
-        self.dyn_model = dyn_model
-        self.sample_size = sample_size
-        self.env_conf = env_conf
-
-    def get_random_actions(self, action_type):
-        actions = np.empty(
-            shape=(self.sample_size, self.env_conf['action_dim']))
-        actions[:, 0] = action_type
-        actions[:, 1] = np.random.uniform(
-            low=0, high=self.env_conf['action_hold_time_limit'],
-            size=(self.sample_size,))
-        return actions
-
-    def get_action(self, state):
-        actions = np.concatenate([
-            self.get_random_actions(action_type)
-            for action_type in range(self.env_conf['action_type_size'])
-        ], axis=0)
-        observations = np.empty(
-            shape=(self.sample_size * self.env_conf['action_type_size'],
-                   self.env_conf['observation_dim']))
-        observations[:, :] = state
-        nobs, rewards = self.dyn_model.predict(observations, actions)
-        '''
-        select mpc optimal action
-        for rewards, we perfer which reward is > 0
-        for action_type and hold_time, we perfer samller one
-        '''
-        return actions[np.argmax(nobs[:, -1])]
-        if np.sum(rewards > 0) == 0:
-            # if game will fail no matter which action we take
-            # return a random action
-            return actions[np.random.randint(0, actions.shape[0] + 1)]
-
-        actions = actions[rewards > 0, :]
-        actions.sort(axis=0)
-        return actions[0]
-
-
-class TexRunnerBrain:
-
-    def __init__(self, params):
-        self.params = params
+class TrainJob:
--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+import json
+import numpy as np
 
 
 def build_mlp(input_placeholder,
@@ -17,3 +19,47 @@ def build_mlp(input_placeholder,
                 out, size, activation=activation, kernel_regularizer=kernel_regularizer)
         out = tf.layers.dense(out, output_size, activation=output_activation)
     return out
+
+
+def load_sample_data(path):
+    with open(path, 'r') as f:
+        return json.loads(f.read())
+
+
+DATA_BUFFER_LIMIT = 50000
+
+
+class DataBuffer:
+
+    def __init__(self, init_data, limit=DATA_BUFFER_LIMIT):
+        # YOU SHOULDN'T MODIFY BUFFER LIMIT AT ANY TIME
+        self._limit = limit
+        self._data = np.empty(shape=(self._limit, ), dtype=np.object)
+        self._tail = 0
+        self.add_data(init_data)
+
+    def sample(self, sample_size):
+        indices = np.arange(self._tail)
+        np.random.shuffle(indices)
+        return self._data[indices[:sample_size]]
+
+    def add_data(self, new_data):
+        new_data = np.array(new_data)
+        if new_data.shape[0] > self._limit:
+            print('Fatal Error! Data is too large to add to data buffer')
+            return
+        if self._tail + new_data.shape[0] > self._limit:
+            slots_need_to_delete = new_data.shape[0] - \
+                (self._limit - self._tail)
+            remain_slots = self._tail - slots_need_to_delete
+            self._data[:remain_slots] = self._data[slots_need_to_delete:self._tail]
+            self._tail -= slots_need_to_delete
+
+        self._data[self._tail:self._tail + new_data.shape[0]] = new_data
+        self._tail += new_data.shape[0]
+
+    def __str__(self):
+        return 'DataBuff with limit %d: ' % self._limit + str(self._data)
+
+    def __repr__(self):
+        return str(self)
