ec3863f4868666d8a014da76238a56378da67e29
rolight <flsnnx@gmail.com>
2018-04-09 21:55:51 +0800

    train code finished

--- train.py
+++ train.py
@@ -3,12 +3,15 @@ import tensorflow as tf
 import pickle
 import json
 import numpy as np
+import os
+from pathlib import Path
 
 from models import NNDynamicModel, MPCcontroller
-from utils import load_sample_data, DataBuffer
+from utils import load_sample_data, DataBuffer, collect_sample_with_mpc
+from game_controller import GameEnv
 
-DYNMAMIC_MODEL_PARAMS = {
-    'regularizer': tf.contrib.layers.l2_regularizer(scale=1e-5),
+DYNAMIC_MODEL_PARAMS = {
+    'kernel_regularizer': tf.contrib.layers.l2_regularizer(scale=1e-5),
     'activation': tf.nn.relu,
     'output_activation': None,
     'learning_rate': 0.003,
@@ -18,8 +21,150 @@ DYNMAMIC_MODEL_PARAMS = {
     'iterations': 40
 }
 
+SAMPLE_DATA_PATH = './sample.json'
+DATABUFFER_LIMIT = 50000
+
+TRAIN_PARAMETERS = {
+    'sample_size': 30000,
+    'collect_data_size': 8000,
+    'collect_threads': 4
+}
+
 
 now_datetime = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
 
 
 class TrainJob:
+
+    def __init__(self, name=now_datetime):
+        self.name = name
+        self.sess = tf.Session()
+        if not self.can_load():
+            self.init_data_buffer()
+            self.init_dyn_model()
+            self.init_train_params()
+        else:
+            self.load()
+            self.load_data_buffer()
+            self.load_dyn_model()
+        self.init_mpc_controller()
+
+    def init_train_params(self):
+        self.params = TRAIN_PARAMETERS
+
+    def init_data_buffer(self):
+        '''
+            Init a new data buffer.
+            Preload random sample from sample.json.
+            If you don't have this file, you need to run generate_random_sample to create one
+        '''
+        init_data = load_sample_data(SAMPLE_DATA_PATH)
+        self.db = DataBuffer(init_data, DATABUFFER_LIMIT)
+
+    def init_dyn_model(self):
+        '''
+            Init a new dyn model with DYNAMIC_MODEL_PARAMS
+        '''
+        self.dyn_model_params = DYNAMIC_MODEL_PARAMS
+        self.dyn_model = NNDynamicModel(
+            name=self.name, sess=self.sess, **self.dyn_model_params)
+
+    def init_mpc_controller(self):
+        '''
+            Init mpc controller
+        '''
+        self.mpc = MPCcontroller(dyn_model=self.dyn_model)
+
+    def save_dyn_model(self):
+        print('Saving dyn_model to disk...')
+        # create path
+        path = Path('./%s/dyn_model/' % self.name)
+        path.mkdir(parents=True, exist_ok=True)
+        # saving model
+        saver = tf.train.Saver()
+        save_path = saver.save(
+            self.sess, './%s/dyn_model/model.ckpt' % self.name)
+        print('Model has saved in %s' % save_path)
+        # saving params
+        with open('./%s/dyn_model/params.pkl' % self.name, 'wb') as f:
+            pickle.dump(self.dyn_model_params, f)
+        print('Model parameter has saved in %s' %
+              ('./%s/dyn_model/params.pkl' % self.name))
+
+    def save_dataBuffer(self):
+        print('Saving databuffer to disk...')
+        path = Path('./%s/' % self.name)
+        path.mkdir(parents=True, exist_ok=True)
+        with open('./%s/data_buffer.pkl', 'wb') as f:
+            pickle.dump(self.db, f)
+        print('DataBuffer has saved to %s' %
+              ('./%s/data_buffer.pkl' % self.name))
+
+    def save(self):
+        print('Savning training parameters to disk...')
+        path = Path('./%s/' % self.name)
+        path.mkdir(parents=True, exist_ok=True)
+        self.params['name'] = self.name
+        with open('./%s/parameters.pkl', 'wb') as f:
+            pickle.dump(self.params, f)
+        print('Training parameters has saved to %s' %
+              ('./%s/parameters.pkl' % self.name))
+
+    def can_load(self):
+        path = Path('./%s/' % self.name)
+        return path.exists()
+
+    def load_data_buffer(self):
+        print('Loading databuffer from disk...')
+        with open('./%s/data_buffer.pkl', 'rb') as f:
+            self.db = pickle.load(f)
+        print('DataBuffer has loaded from %s' %
+              ('./%s/data_buffer.pkl' % self.name))
+
+    def load_dyn_model(self):
+        print('Loading dyn_model from disk...')
+        # loading params
+        with open('./%s/dyn_model/params.pkl' % self.name, 'rb') as f:
+            self.dyn_model_params = pickle.dump(f)
+        print('Model parameter has loaded from %s' %
+              ('./%s/dyn_model/params.pkl' % self.name))
+        # building graph
+        self.dyn_model = NNDynamicModel(
+            sess=self.sess, **self.dyn_model_params)
+        # restore model
+        saver = tf.train.Saver()
+        saver.restore(self.sess, './%s/dyn_model/model.ckpt' % self.name)
+
+    def load(self):
+        print('Loading training parameters from disk...')
+        with open('./%s/parameters.pkl', 'rb') as f:
+            self.params = pickle.load(f)
+        print('Training parameters has loaded from %s' %
+              ('./%s/parameters.pkl' % self.name))
+
+    def train_loop(self):
+        '''
+            step 1. Train dynamic model with sample data.
+                    After training, save model to disk.
+        '''
+        data = self.db.sample(sample_size=self.params['sample_size'])
+        self.dyn_model.fit(data)
+        self.save_dyn_model()
+        '''
+            step 2. Run game and using MPC to collect new data.
+                    Add new data to DataBuffer.
+                    Save DataBuffer to disk
+        '''
+        data = collect_sample_with_mpc(
+            num_samples=self.params['collect_data_size'],
+            num_threads=self.params['collect_threads'])
+        self.db.add_data(data)
+        self.save_dataBuffer()
+
+    def train(self):
+        if not self.dyn_model.initialized:
+            initialize = tf.global_variables_initializer()
+            self.sess.run(initialize)
+        while True:
+            self.train_loop()
+            self.save()
--- utils.py
+++ utils.py
@@ -1,6 +1,11 @@
 import tensorflow as tf
 import json
 import numpy as np
+import pickle
+import os
+import time
+
+from multiprocessing import Process
 
 
 def build_mlp(input_placeholder,
@@ -26,19 +31,16 @@ def load_sample_data(path):
         return json.loads(f.read())
 
 
-DATA_BUFFER_LIMIT = 50000
-
-
 class DataBuffer:
 
-    def __init__(self, init_data, limit=DATA_BUFFER_LIMIT):
+    def __init__(self, init_data, limit):
         # YOU SHOULDN'T MODIFY BUFFER LIMIT AT ANY TIME
         self._limit = limit
         self._data = np.empty(shape=(self._limit, ), dtype=np.object)
         self._tail = 0
         self.add_data(init_data)
 
-    def sample(self, sample_size):
+    def sample(self, sample_size=None):
         indices = np.arange(self._tail)
         np.random.shuffle(indices)
         return self._data[indices[:sample_size]]
@@ -63,3 +65,96 @@ class DataBuffer:
 
     def __repr__(self):
         return str(self)
+
+
+def _collect_sample_with_mpc_thread(num_samples, output, train_job_name):
+    from game_controller import GameEnv
+    from models import NNDynamicModel, MPCcontroller
+    import tensorflow as tf
+
+    '''
+        step 1. make a new dyn_model and load add all parameters
+    '''
+    sess = tf.Session()
+    print('Loading dyn_model from disk...')
+    # loading params
+    with open('./%s/dyn_model/params.pkl' % train_job_name, 'rb') as f:
+        dyn_model_params = pickle.dump(f)
+    print('Model parameter has loaded from %s' %
+          ('./%s/dyn_model/params.pkl' % train_job_name))
+    # building graph
+    dyn_model = NNDynamicModel(
+        sess=sess, **dyn_model_params)
+    # restore model
+    saver = tf.train.Saver()
+    saver.restore(sess, './%s/dyn_model/model.ckpt' % train_job_name)
+
+    '''
+        step 2. init a new mpc controller with dyn_model
+    '''
+
+    mpc = MPCcontroller(dyn_model=dyn_model)
+
+    '''
+        step 3. create a new env and collect samples
+    '''
+    print_every = 100
+    env = GameEnv()
+    sample_count = 0
+    env.start()
+    last_obs = env.get_observation()
+    obs = None
+    samples = []
+    while sample_count < num_samples:
+        while last_obs is None:
+            time.sleep(env.interval_time)
+            last_obs = env.get_observation()
+        # using mpc controller to get action
+        action = mpc.get_action(last_obs)
+        obs, done, reward = env.perform_action(action)
+        if obs is None:
+            # Taken last operation make game almost failure, we just mark it was done
+            # and mark last sample's reward to be -1
+            done = True
+            samples[-1][-1] = -1
+        else:
+            samples.append(
+                [last_obs.tolist(), action.tolist(), obs.tolist(), reward])
+            sample_count += 1
+        if done:
+            env.restart()
+            last_obs = None
+        else:
+            last_obs = obs
+
+        if sample_count % print_every == 0:
+            print('Process %s have collected %d samples.' %
+                  (output, sample_count))
+    with open(output, 'w') as f:
+        f.write(json.dumps(samples))
+
+
+def collect_sample_with_mpc(num_samples, num_threads, train_job_name):
+    num_samples_each_thread = num_samples // num_threads
+    processes = []
+    for pid in range(num_threads):
+        if pid == num_threads - 1:
+            num_samples_each_thread += num_samples % num_threads
+        p = Process(target=_collect_sample_with_mpc_thread, args=(
+            num_samples_each_thread, 'sample_process_%d' % pid, train_job_name))
+        processes.append(p)
+
+    for p in processes:
+        p.start()
+
+    for p in processes:
+        p.join()
+
+    print('all process finished, collecting data from output file...')
+    all_samples = []
+    for pid in range(num_threads):
+        with open('sample_process_%d' % pid, 'r') as f:
+            cur_samples = json.loads(f.read())
+            all_samples += cur_samples
+        os.remove('sample_process_%d' % pid)
+    return all_samples
